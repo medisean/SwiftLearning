818b5146b23116b01d89dccb4b237563ef165185
liangmingzou@163.com <liangmingzou@163.com>
2016-04-09 13:51:24 +0800

    Add cycle class demo

---
+++ ResolvingCycleClassDemo.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+// Resolving Strong reference cycles between class reference
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Person {
+    let name: String
+    init(name: String) { self.name = name }
+    var apartment: Apartment?
+    deinit {
+        print("Person is being deinitialized")
+    }
+}
+
+class Apartment {
+    let unit: String
+    init(unit: String) { self.unit = unit }
+    weak var tenant: Person?
+    deinit {
+        print("Apartment is being deinitialized")
+    }
+}
+
+var p: Person?
+var a: Apartment?
+
+p = Person(name: "Joson")
+a = Apartment(unit: "Luocheng Road")
+p?.apartment = a
+a?.tenant = p
+
+//p = nil
+a = nil
+print(p?.apartment?.unit)
+
+
